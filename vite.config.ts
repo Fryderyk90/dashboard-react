3c9a159e84ddd4b1e8d75d54f7beaba084b3574a
Frydeyk <65290472+Frydeyk@users.noreply.github.com>
2024-01-27 13:49:17 +0100

    add target

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,9 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  esbuild: {
-    format: 'cjs', // Use CommonJS format
-  },
   build:{
-    outDir: 'dist',
+    target: 'esnext', // Ensure that Vite is configured to output ES modules
+    outDir: './dist',
   },
   resolve: {
     alias: {
